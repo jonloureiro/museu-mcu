b76fbb9436257702729943105f5c88a6e00e2737
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-19 15:01:17 -0400

    conectando no wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
 #include <Arduino.h>
 #include <WiFiClientSecure.h>
 
+void connectWiFi();
+
+const char* ssid         = "YOURSSID";
+const char* password     = "YOURPASSWORD";
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
 
-  Serial.println("Test");
+  connectWiFi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+void connectWiFi() {
+  Serial.print("\nAttempting to connect to SSID ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+
+  for(int i = 0; i < 10; i++) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.print("\nConnected");
+      return;
+    }
+    Serial.print(".");
+    delay(500);
+  }
+
+  connectWiFi();
+}
+
